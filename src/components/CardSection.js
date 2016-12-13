964e329ec3e452155b7ba7698140ac420d32ec51
Ian Wilson <iwilsonq@gmail.com>
2016-12-13 14:48:44 -0800

    wired to ruffr-server

---
+++ src/components/CardSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardSection = props => {
+  return (
+    <div className="card-section">
+      {props.children}
+    </div>
+  );
+};
+
+export default CardSection;
--- src/components/PetsContainer.js
+++ src/components/PetsContainer.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import Card from './Card';
+import CardSection from './CardSection';
 
 class PetsContainer extends Component {
+  state = {
+    pets: []
+  };
+
+  componentDidMount() {
+    Axios.get('http://localhost:4000/pets')
+      .then(results => results.data)
+      .then(pets => this.setState({ pets }));
+  }
+
+  renderCards() {
+    const { name, pictures } = this.state.pets;
+
+    return this.state.pets.map((pet, i) =>
+      <Card key={i}>
+        <CardSection>
+          <img src={`http://localhost:4000/images/${pet.pictures[0]}`} alt={`${name}`} />
+        </CardSection>
+        <CardSection>
+          <h3>{name}</h3>
+        </CardSection>
+      </Card>
+    );
+  }
+
   render() {
     return (
       <div className="pets-container">
-        <div className="pet">pet01</div>
-        <div className="pet">pet02</div>
-        <div className="pet">pet03</div>
-        <div className="pet">pet04</div>
-        <div className="pet">pet05</div>
-        <div className="pet">pet06</div>
+        {this.renderCards()}
       </div>
     );
   }
